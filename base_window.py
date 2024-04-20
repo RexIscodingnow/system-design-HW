266e2e5691fc68516fa80e81f3f3db1876d5356d
Rex <a0916996002@gmail.com>
2024-04-20 15:24:53 +0800

    Add: template for the base window inheritance

---
+++ base_window.py
@@ -0,0 +1,132 @@
+import tkinter as tk
+from tkinter import messagebox
+
+import pathlib
+from PIL import Image, ImageTk
+from collections import namedtuple
+
+
+
+class BaseWindow(tk.Tk):
+    def __init__(self,
+                 win_title: str | None = None,
+                 width: int = 500,
+                 height: int = 600,
+                 *,
+                 min_width: int = 0,
+                 min_height: int = 0,
+                 minSize: tuple[int] | None = None
+                 ) -> None:
+        """
+        Parameters:
+            - win_title: Set the title of the window
+
+            - width: The width of the window size
+
+            - height: The height of the window size
+
+            - min_width: The minimum width of window size
+
+            - min_height: The minimum height of window size
+
+            - minSize: The first element represents width, and the second represents the height.
+                       It sets both similar to `min_width` and `min_height`.
+            
+        Properties:
+            - image_path: Given an image path for background.
+                          It can use relative path, absolute path to read it.
+            
+            >>> image_path = 'C:/Users/usr/images/abc.jpg'
+            >>> image_path = './images/abc.jpg'
+        """
+        
+        super().__init__()
+
+        self.config(
+            bg="black"
+        )
+        self.width = width
+        self.height = height
+        self.wm_title(win_title)
+        self.old_size = namedtuple("old_size", ("width", "height"))
+        self.old_size.width = self.winfo_width()
+        self.old_size.height = self.winfo_height()
+        
+        # ===========================================================
+        # ===========================================================
+
+        """ set window size """
+        
+        self.geometry(f"{self.width}x{self.height}")
+        
+        if minSize is None:
+            self.minsize(min_width, min_height)
+        else:
+            self.minsize(*minSize)
+        
+
+        # ===========================================================
+        # ===========================================================
+
+        """ background image """
+        
+        self.__img_path = ""
+        self.canvas = tk.Canvas(self, width=self.winfo_screenwidth(), height=self.winfo_screenheight())
+    
+
+    @property
+    def image_path(self):
+        return self.__img_path
+    
+    @image_path.setter
+    def image_path(self, new_img_path: str):
+        try:
+            # self.__img_path = os.path.join(os.getcwd(), new_img_path)
+            self.__img_path = pathlib.Path(new_img_path)
+            # print(self.__img_path)
+            self.bg_img = ImageTk.PhotoImage(Image.open(self.__img_path))
+            self.canvas.create_image(0, 0, anchor=tk.NW, image=self.bg_img)
+            self.canvas.place(x=0, y=0)
+        
+        except:
+            messagebox.showerror(
+                "Error Message",
+                "提示：圖片載入出錯，請檢查該檔案是否存在該路徑"
+            )
+
+    
+    def centerToScreen(self) -> None:
+        """
+        Position the window at the center screen coordinates.
+        """
+        screen_width = self.winfo_screenwidth()
+        screen_height = self.winfo_screenheight()
+        center_x = (screen_width - self.width) // 2
+        center_y = (screen_height - self.height) // 2
+
+        self.geometry(f"{self.width}x{self.height}+{center_x}+{center_y}")
+
+
+def all_widgets_map(window: BaseWindow) -> dict[str, tk.Widget]:
+    """
+    Return a dictionary of all widget names and objects in the specified window
+    
+    Parameter
+        - window: target window of Tk
+
+    rtype: dictionary
+            - key: Name of target widget
+            - value: Widget
+    """
+    all_widgets = {}
+
+    for widget in window.winfo_children():
+        widget_name = widget.winfo_name()
+        all_widgets[widget_name] = widget
+
+    return all_widgets
+
+
+
+
+
