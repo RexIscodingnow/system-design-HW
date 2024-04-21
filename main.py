bfd284d7acaedbca0ebdd44760586abfbdbf2da4
Rex <a0916996002@gmail.com>
2024-04-21 21:52:51 +0800

    Add: Login window shows the placeholder message in the textboxes

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import time
 import pathlib
-from base_window import BaseWindow
+from base_window import BaseWindow, all_widgets_map
 
 import tkinter as tk
 from tkinter import messagebox
@@ -28,68 +28,64 @@ class LoginWindow(BaseWindow, tk.Toplevel):
 
         self.resizable(False, False)
         self.centerToScreen()
-        self.PLACEHOLDER_EMAIL = "輸入電子信箱 :"
-        self.PLACEHOLDER_USERNAME = "輸入使用者名稱 :"
-        self.PLACEHOLDER_PASSWORD = "輸入密碼 :"
+        
+        # ==============================================================
 
+        # 輸入框顯示訊息
+        PLACEHOLDER_EMAIL = "輸入電子信箱"
+        PLACEHOLDER_USERNAME = "輸入使用者名稱"
+        PLACEHOLDER_PASSWORD = "輸入密碼"
 
-        # ==============================================
-        # ==============================================
+        # ==============================================================
 
+        # 所有元件設置
         lbl_title = tk.Label(self, font=FONT_TEXT, text="登入帳號")
         self.entry_email = tk.Entry(self, font=FONT_ENTRY)
         self.entry_username = tk.Entry(self, font=FONT_ENTRY)
-        self.entry_password = tk.Entry(self, font=FONT_ENTRY)
+        self.entry_password = tk.Entry(self, font=FONT_ENTRY, name="entry-pwd")
         self.submit = tk.Button(self, font=FONT_BTN, text="登入")
 
-        self.entry_email.insert(0, self.PLACEHOLDER_EMAIL)
-        self.entry_username.insert(0, self.PLACEHOLDER_USERNAME)
-        self.entry_password.insert(0, self.PLACEHOLDER_PASSWORD)
 
         lbl_title.place(
-            x=self.winfo_width() + 190, y=10
+            x = self.winfo_width() + 190,    y =  10
         )
         self.entry_email.place(
-            x=self.winfo_width() + 100, y=80
+            x = self.winfo_width() + 100,    y =  80
         )
         self.entry_username.place(
-            x=self.winfo_width() + 100, y=130
+            x = self.winfo_width() + 100,    y = 130
         )
         self.entry_password.place(
-            x=self.winfo_width() + 100, y=180
+            x = self.winfo_width() + 100,    y = 180
         )
         self.submit.place(
-            x=self.winfo_width() + 210, y=230
+            x = self.winfo_width() + 210,    y = 230
         )
 
-        self.grid_columnconfigure(0, weight=1)
+        self.entry_email.insert(0, PLACEHOLDER_EMAIL)
+        self.entry_email.configure(state='disabled')
+        self.entry_username.insert(0, PLACEHOLDER_USERNAME)
+        self.entry_username.configure(state='disabled')
+        self.entry_password.insert(0, PLACEHOLDER_PASSWORD)
+        self.entry_password.configure(state='disabled')
+
 
         # ==============================================
 
-        self.bind('<FocusIn>', lambda event: self.focus_in())
-        self.bind('<FocusOut>', lambda event: self.focus_out())
+        self.entry_email.bind('<Button-1>', lambda event: self.focus_in_entry(self.entry_email))
+        self.entry_username.bind('<Button-1>', lambda event: self.focus_in_entry(self.entry_username))
+        self.entry_password.bind('<Button-1>', lambda event: self.focus_in_entry(self.entry_password))
+        
+        # ---------------------------------------------------------
+        
+        self.entry_email.bind('<FocusOut>', lambda event: self.focus_out_entry(self.entry_email, PLACEHOLDER_EMAIL))
+        self.entry_username.bind('<FocusOut>', lambda event: self.focus_out_entry(self.entry_username, PLACEHOLDER_USERNAME))
+        self.entry_password.bind('<FocusOut>', lambda event: self.focus_out_entry(self.entry_password, PLACEHOLDER_PASSWORD))
+
+        # self.widgets_mapping = all_widgets_map(self)
+        # print(self.widgets_mapping)
 
     
-    def focus_out(self):
-        if not self.entry_email.get():
-            self.entry_email.insert(0, self.PLACEHOLDER_EMAIL)
-
-        if not self.entry_username.get():
-            self.entry_username.insert(0, self.PLACEHOLDER_USERNAME)
-
-        if not self.entry_password.get():
-            self.entry_password.insert(0, self.PLACEHOLDER_PASSWORD)
-
-
-    def focus_in(self):
-        if self.entry_email.get() == self.PLACEHOLDER_EMAIL:
-            self.entry_email.delete(0, "end")
-
-        if self.entry_username.get() == self.PLACEHOLDER_USERNAME:
-            self.entry_username.delete(0, "end")
-
-        if self.entry_password.get() == self.PLACEHOLDER_PASSWORD:
-            self.entry_password.delete(0, "end")
 
 
 class MainWindow(BaseWindow):
