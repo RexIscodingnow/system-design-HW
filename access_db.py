794faa2bdaf974be8996165c6118ee8e4d1fe51b
Rex <a0916996002@gmail.com>
2024-04-27 10:10:44 +0800

    Add: select() => handle the 'WHERE' syntax

--- access_db.py
+++ access_db.py
@@ -47,7 +47,12 @@ def checkLength(params: dict[str, Iterable[int | float | str | object | None]]) 
 def exec_cmd_sql(sql_cmd: str, values: Iterable = [], *, db_path: str = "users.db", exe_many: bool = False):
     """
     Execute the sqlite script or command (one-line or multiple command)
-    
+
+
+    Return values:
+        
+        - tuple: 1. First element is a result fetched from the target table.
+                 2. Secondly is the length of the fetch result.
     
     Parameters:
 
@@ -69,9 +74,28 @@ def exec_cmd_sql(sql_cmd: str, values: Iterable = [], *, db_path: str = "users.d
                     `password`  VARCHAR(20)     NOT NULL,
             );
 
-        - values: 
+        
+        - values: The parameters where sqlite script / command to use.
+
+        >>> values = ['Peter', 'Computer Science', 80]
+        
+            insert into `students`
+            (`name`, `major`, `score`)
+            values
+            (?, ?, ?)
+
+            It's equivalent to
+
+            insert into `students`
+            (`name`, `major`, `score`)
+            values
+            ('Peter', 'Computer Science', 80)
+
+        - db_path: The database path
+        - exe_many: To execute multiple SQL command if it is set to `True`.
     """
     try:
+        res_fetch = []
         conn = sqlite3.connect(pathlib.Path(db_path))
         cursor = conn.cursor()
 
@@ -108,14 +132,15 @@ def insert(table_name: str, params: dict[str, Iterable[int | float | str | objec
         - params: 插入欄位資料
 
             * insert columns `name`, `email`, `score`
-            * 3 筆資料
+                
+                * 2 筆資料
             >>> {
                     'name': ('大黃', '小黑'),
                     'email': ('abc@gmail.com', 'black@email.com'),
                     'score': (90, 65)
                 }
             
-            Users table:
+        Example `Users` table:
             
                     +--------+-----------------+-------+
                     |  name  |      email      | score |
@@ -168,10 +193,10 @@ def insert(table_name: str, params: dict[str, Iterable[int | float | str | objec
 
 
 def select(table_name: str,
-           fields: Iterable[str] = [],
-           search_vals: Iterable[int | float | str | object] = [],
+           search_vals: Iterable[tuple[str, int | float | str | object]] = [],
            conditions: Iterable[str] = [],
            *,
+           fetch_columns: Iterable[int | float | str | object] = [],
            aggregation: dict[str, Iterable[str]] = {}
            ) -> Iterable[int | float | str | object] | None:
     """
@@ -179,21 +204,57 @@ def select(table_name: str,
         
         - table_name: 資料表名稱
 
-        - fields: 資料表的欄位名稱
+        - search_vals: 資料表的欄位名稱 + 搜尋資料
+                             
+            - Explanation: 第一個為欄位名稱，第二個為判斷資料
 
-        - search_vals: 要搜尋的參數
+        >>> [('name', 'John')]
+
+            SELECT * FROM `users` WHERE `name` <condition 1> `John`;
         
+        >>> [('name', 'John'), ('score', '90')]
+
+            SELECT * FROM `users`
+            WHERE
+            `name` <condition 1> `John`
+            <condition 2>
+            `score` <condition 2> 90 
+
+        - conditions: 條件判斷運算子
+
+        以 [('name', 'John'), ('score', '90')] 為例子，由左至右
+
+        1. '='   : 對應到第一個條件 => ('name', 'John')
+
+        2. 'and' : 兩個條件做 and
+        
+        3. '>'   : 對應第二個       => ('score', '90')
+
+        >>> ['=', 'and', '>']
+
+        最後指令為  
+                    SELECT * FROM `users`
+                    WHERE
+                    `name` = `John`
+                    AND
+                    `score` > 90
+
+        
+        - fetch_columns: 搜尋的指令欄位
+
         - aggregation: 聚合函數 (Aggregation function)
         
         >>> {
-                "<Column name>" : "<Aggregation function>"
+                "<Aggregation function>" : "<Column name>"
             }
 
+        * 對 score 的資料做排序、計算數量
         >>> {
-                "score": ["sort", "count"]   # 對 score 的資料做排序、計算數量
+                "sort": score,
+                "count": score
             }
 
-            Users table:
+        Example `Users` table:
 
                     +---------+-----------------+--------+
                     |  name   |      email      | score  |
@@ -203,28 +264,13 @@ def select(table_name: str,
                     |  Emily  | black@email.com |   65   |
                     +---------+-----------------+--------+
     """
-    
-    if len(fields) != len(search_vals):
-        return None
 
     columns = ""
-    where = ""
-    # conditions = {
-    #     "equal": "=",
-    #     "not-eq": "<>",
-    #     "greater": ">",
-    #     "less": "<",
-    #     "greater-eq": ">=",
-    #     "less-eq": "<="
-    # }
 
-    if len(fields) > 0:
-        for i in range(len(fields)):
-            item = fields[i]
-            columns += item[0]
+    n = len(fetch_columns)
 
-            if i < len(fields) - 2:
-                columns += ","
+    if fetch_columns:
+        columns = ','.join([f"`{str(col)}`" for col in fetch_columns])
     else:
         columns = "*"
 
@@ -232,9 +278,37 @@ def select(table_name: str,
     # 
     # <conditions> : <column name> < =, <> (not equal), >, <, between, like, in > <value>
 
-    cmd = f"select {columns} from {table_name}"
+    sql_cmd = f"SELECT {columns} FROM `{table_name}`"
+
+    # TODO: test the edge case
+    if search_vals and conditions:
+        if len(search_vals) > 1 and len(search_vals) == len(conditions):
+            print("判斷條件 與 運算子 數目不一致")
+            return
+
+        sql_cmd += " WHERE "
+
+        for i, (col, val) in enumerate(search_vals):
+            if i > 0:
+                sql_cmd += f" {conditions[i]} "
+                continue
+
+            sql_cmd += f"`{col}` {conditions[i]} '{val}'"
+
+        if len(search_vals) > 1:
+            sql_cmd += f"`{col}` {conditions[i+1]} '{val}'"
+
+
+    # TODO: aggregation functions
+    if aggregation:
+        for col, funcs in aggregation.items():
+            for func in funcs:
+                pass
+
+    print(sql_cmd)
     
-    if where != "":
-        cmd += f" where {where} "
+    res = exec_cmd_sql(sql_cmd)
+
+    return res
 
 
